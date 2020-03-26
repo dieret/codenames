bf7c1112f43f75712bf7d88defbfc07971058100
Kilian Lieret <kilian.lieret@posteo.de>
2020-03-26 15:54:42 +0100

    Enter message box should appear above chat messages

--- codenames/messages.py
+++ codenames/messages.py
@@ -26,5 +26,5 @@ class Messages(object):
         self.messages.append(message)
 
     def to_html(self):
-        return "<br/>".join([m.to_html() for m in self.messages])
+        return "<br/>".join([m.to_html() for m in reversed(self.messages)])
 
