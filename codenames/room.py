6b6f86a49060f3efc18f1c73459c570654aa0903
Alvaro de Diego <dediego@ma.tum.de>
2020-05-08 11:05:18 +0200

    added chat message informing about room number

--- codenames/room.py
+++ codenames/room.py
@@ -1,16 +1,17 @@
 from codenames.users import Users
-from codenames.messages import Messages
+from codenames.messages import Messages, Message
 from codenames.playground import Playground
 
 class Room(object):
     def __init__(self, number):
-        self.restart()
         self.number = number
+        self.restart()
 
     def restart(self):
         self.playground = Playground.generate_new()
         self.users = Users()
         self.messages = Messages()
+        self.messages.add_message(Message("You are in room {number}".format(number = self.number+1)))
 
 
     
